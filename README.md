3968bdb9f8820027969eddc524fbeb25c5925708
thiagofranchin <thiago.franchin@gmail.com>
2022-10-13 19:40:27 -0300

    Lab design system

---
+++ README.md
@@ -0,0 +1,1 @@
+[Notion Documentation](https://www.notion.so/React-Vite-Storybook-6394d94a317948caa7ed7e433c4c2fda)
---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,14 @@
+import * as CheckboPrimitive from '@radix-ui/react-checkbox';
+import { Check } from 'phosphor-react';
+
+export interface CheckboxProps {}
+  
+export function Checkbox({}: CheckboxProps) {
+  return(
+    <CheckboPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+      <CheckboPrimitive.Indicator asChild>
+        <Check weight='bold' className='h-5 w-5 text-cyan-500' />
+      </CheckboPrimitive.Indicator>
+    </CheckboPrimitive.Root>
+  )
+}
